1c3c2a5af5d18e5a33374e7eb8bf3775c8db82fd
Natalia <natinho@yandex.ru>
2022-03-07 01:29:30 +0300

    Test in process

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -13,41 +13,41 @@ class MainKtTest {
 //        assertEquals(sumMastercard, result)
 //    }
 
-    @Test
-    fun sumPreviousTransferMaestro() {
-        val typeCard = "Maestro"
-        val result = sumPreviousTransfer(
-            typeOfCard = typeCard
-        )
-        assertEquals(sumMaestro, result)
-    }
+//    @Test
+//    fun sumPreviousTransferMaestro() {
+//        val typeCard = "Maestro"
+//        val result = sumPreviousTransfer(
+//            typeOfCard = typeCard
+//        )
+//        assertEquals(sumMaestro, result)
+//    }
 
-    @Test
-    fun sumPreviousTransferVisa() {
-        val typeCard = "Visa"
-        val result = sumPreviousTransfer(
-            typeOfCard = typeCard
-        )
-        assertEquals(sumVisa, result)
-    }
+//    @Test
+//    fun sumPreviousTransferVisa() {
+//        val typeCard = "Visa"
+//        val result = sumPreviousTransfer(
+//            typeOfCard = typeCard
+//        )
+//        assertEquals(sumVisa, result)
+//    }
 
-    @Test
-    fun sumPreviousTransferMir() {
-        val typeCard = "Mir"
-        val result = sumPreviousTransfer(
-            typeOfCard = typeCard
-        )
-        assertEquals(sumMir, result)
-    }
+//    @Test
+//    fun sumPreviousTransferMir() {
+//        val typeCard = "Mir"
+//        val result = sumPreviousTransfer(
+//            typeOfCard = typeCard
+//        )
+//        assertEquals(sumMir, result)
+//    }
 
-    @Test
-    fun sumPreviousTransferVkPay() {
-        val typeCard = "vkPay"
-        val result = sumPreviousTransfer(
-            typeOfCard = typeCard
-        )
-        assertEquals(sumVkPay, result)
-    }
+//    @Test
+//    fun sumPreviousTransferVkPay() {
+//        val typeCard = "vkPay"
+//        val result = sumPreviousTransfer(
+//            typeOfCard = typeCard
+//        )
+//        assertEquals(sumVkPay, result)
+//    }
 
     @Test
     fun main() {
@@ -79,44 +79,103 @@ class MainKtTest {
     @Test
     fun setSumMastercard() {
         val typeCard = "Mastercard"
-        val sumTransferTest = 20_000_00
+        val sumTransferTest : Long = 20_000_00
 
         val result = sumPreviousAfterTransfer(
-            //typeOfCard = typeCard,
+            typeOfCard = typeCard,
+            sumTransfer = sumTransferTest
 
         )
+
+        assertEquals(testGetSumMastercard(), result)
     }
 
     @Test
     fun getSumMaestro() {
+        val typeCard = "Maestro"
+        val result = sumPreviousTransfer(
+            typeOfCard = typeCard
+        )
+        assertEquals(sumMaestro, result)
     }
 
     @Test
     fun setSumMaestro() {
+        val typeCard = "Maestro"
+        val sumTransferTest : Long = 20_000_00
+
+        val result = sumPreviousAfterTransfer(
+            typeOfCard = typeCard,
+            sumTransfer = sumTransferTest
+        )
+
+        assertEquals(getSumMaestro(), result)
     }
 
     @Test
     fun getSumVisa() {
+        val typeCard = "Visa"
+        val result = sumPreviousTransfer(
+            typeOfCard = typeCard
+        )
+        assertEquals(sumVisa, result)
     }
 
     @Test
     fun setSumVisa() {
+        val typeCard = "Visa"
+        val sumTransferTest : Long = 20_000_00
+
+        val result = sumPreviousAfterTransfer(
+            typeOfCard = typeCard,
+            sumTransfer = sumTransferTest
+        )
+
+        assertEquals(getSumVisa(), result)
     }
 
     @Test
     fun getSumMir() {
+        val typeCard = "Mir"
+        val result = sumPreviousTransfer(
+            typeOfCard = typeCard
+        )
+        assertEquals(sumMir, result)
     }
 
     @Test
     fun setSumMir() {
+        val typeCard = "Mir"
+        val sumTransferTest : Long = 20_000_00
+
+        val result = sumPreviousAfterTransfer(
+            typeOfCard = typeCard,
+            sumTransfer = sumTransferTest
+        )
+
+        assertEquals(getSumMir(), result)
     }
 
     @Test
     fun getSumVkPay() {
+        val typeCard = "vkPay"
+        val result = sumPreviousTransfer(
+            typeOfCard = typeCard
+        )
+        assertEquals(sumVkPay, result)
     }
 
     @Test
     fun setSumVkPay() {
+        val typeCard = "vkPay"
+        val sumTransferTest : Long = 15_000_00
+
+        val result = sumPreviousAfterTransfer(
+            typeOfCard = typeCard,
+            sumTransfer = sumTransferTest
+        )
+
+        assertEquals(getSumVkPay(), result)
     }
 
     @Test
@@ -137,25 +196,47 @@ class MainKtTest {
     }
 
     @Test
-    fun calculateMastercardMaestro() {
+    fun calculateMastercardMaestroUnderLimit() {
         val sumPreviousTransferMoney: Long = 0
-        val sumTransferTest = 20_000_00
+        val sumTransferTest : Long = 20_000_00
+
         var sumPreviousTransfer =  sumPreviousTransfer(typeOfCard = "Mastercard")
 
         val result = calculateMastercardMaestro(
-           // sumPreviousTransfer = sumPreviousTransferMoney,
-           // sumTransfer = sumTransferTest
+            sumPreviousTransfer = sumPreviousTransferMoney,
+            sumTransfer = sumTransferTest
         )
         assertEquals(0.0, result)
-
-
     }
+    @Test
+    fun calculateMastercardMaestroUpOfLimit() {
+        val sumPreviousTransferMoney: Long = 0
+        val sumTransferTest : Long = 80_000_00
+        val commissionPercentTest : Double = 0.006
+        val commissionConstantTest : Int = 20_00
+
+        val result = calculateMastercardMaestro(
+            sumPreviousTransfer = sumPreviousTransferMoney,
+            sumTransfer = sumTransferTest,
+        )
+        assertEquals(sumTransferTest * commissionPercentTest + commissionConstantTest
+            , result)
+    }
+
 
     @Test
     fun calculateVisaMir() {
+        val sumTransferTest : Long = 80_000_00
+        val commissionPercent = 0.0075
+        val commissionConstant = 35_00
+
+        val result = calculateVisaMir( sumTransfer = sumTransferTest)
+        assertEquals(sumTransferTest * commissionPercent + commissionConstant, result)
     }
 
     @Test
     fun isAvailableLimits() {
+
+
     }
 }
